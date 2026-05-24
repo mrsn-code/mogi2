acf0973160a5acad7a43e842cd9146fcdbd021ab
mrsn-code <maltifunctionist@gmail.com>
2026-05-25 08:56:34 +0900

    mailhog done

---
+++ src/resources/views/auth/verify-email.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.default')
+@section('css')
+<link rel="stylesheet" href="{{asset('css/verify-email.css')}}">
+@endsection
+
+@section('content')
+<div class="page__wrapper">
+    <p class="verify-email__message">
+        登録していただいたメールアドレスに確認メールを送付しました。<br>
+        メール認証を完了してください。
+    </p>
+    
+    @if (session('status'))
+        <p style="color: green;">
+            {{ session('status') }}
+        </p>
+    @endif
+    <div class="auth__button">
+        <a href="http://localhost:8025" target="_blank" >
+            認証はこちらから
+        </a>
+    </div>
+    
+    <form method="POST" action="{{route('verification.send')}}">
+        @csrf
+        <button class="retransmission__button" type="submit">認証メールを再送する</button>
+    </form>
+</div>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Requests\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('auth.register');
-// });
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->name('verification.notice');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    session()->get('unauthenticated_user')->sendEmailVerificationNotification();
+    session()->put('resent', true);
+    return back()->with('message', 'Verification link sent!');
+})->name('verification.send');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    session()->forget('unauthenticated_user');
+    return redirect('/mypage/profile');
+})->name('verification.verify');
